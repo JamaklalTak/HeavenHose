2fe3c82196c7cf4272bd0f381d4e802c194b9eae
jamak <takjamaklal@gmail.com>
2020-01-20 22:03:59 +0530

    Updates

---
+++ precache-manifest.3b76cdbc2233b6ed29a23a898f25021f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "206b879dc237522b84505dab4b4c1d1b",
+    "url": "/HeavenHose/index.html"
+  },
+  {
+    "revision": "0fd036337e9cde1766ee",
+    "url": "/HeavenHose/static/js/2.1c845bc3.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/HeavenHose/static/js/2.1c845bc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "96288911c4b08eb89b6a",
+    "url": "/HeavenHose/static/js/3.61e9638f.chunk.js"
+  },
+  {
+    "revision": "f7fc9e318d3301889ff9",
+    "url": "/HeavenHose/static/js/4.24afd13e.chunk.js"
+  },
+  {
+    "revision": "225e3623d282e56d7ebd",
+    "url": "/HeavenHose/static/js/main.c4590d76.chunk.js"
+  },
+  {
+    "revision": "56767117612ddf7e4016",
+    "url": "/HeavenHose/static/js/runtime-main.20974f0c.js"
+  }
+]);
